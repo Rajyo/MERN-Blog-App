e57dbee2c49a2e901e35748a286935c2ddef2c1d
Rajyo <yopro1999@gmail.com>
2023-09-13 16:25:26 +0530

    updated axios.js

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ const baseURL = process.env.REACT_APP_BASE_URL;
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
-  timeout: 5000,
+  timeout: 10000,
   headers: {
     Authorization: localStorage.getItem("access_token")
       ? "Bearer " + localStorage.getItem("access_token")
