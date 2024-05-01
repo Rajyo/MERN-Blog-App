14f99b7208fd8bebb6ad78eaac971e07dce55197
Rajyo <yopro1999@gmail.com>
2024-05-01 12:56:44 +0530

    updated code boot note

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,5 +1,3 @@
-import React, { useState, useEffect } from "react";
-import axiosInstance from "../../axios";
 import "./category.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -28,23 +26,7 @@ const SamplePrevArrow = (props) => {
     </div>
   );
 };
-export const Category = () => {
-  const [category, setCategory] = useState([]);
-
-  useEffect(() => {
-    getCategory();
-  }, []);
-
-  const getCategory = async () => {
-    try {
-      await axiosInstance.get(`category`).then((res) => {
-        // console.log(res.data);
-        setCategory(res.data);
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  };
+export const Category = ({category}) => {
 
   const settings = {
     dots: false,
@@ -66,24 +48,7 @@ export const Category = () => {
   };
 
   return (
-    <div className="relative">
-
-      {category.length == 0 &&
-        <div className="absolute z-10 w-full max-h-screen">
-          <section
-            className="bg-black w-[90%] sm:w-[75%] md:w-[60%] lg:w-[50%] m-auto flex flex-col gap-6 p-4 sm:p-8 md:p-10 lg:p-16 rounded-md text-white">
-
-            <h1 className="text-center font-bold text-3xl text-red-600">IMPORTANT!</h1>
-            <h1>You are seeing this message because the Backend Server is hosted on RENDER's FREE Tier Web Service and
-              it spin down after 15 minutes of inactivity.</h1>
-            <h1>Since there are no active users and I am using a Free Service, the first request might take longer to
-              respond.</h1>
-            <h1 className="uppercase text-center text-xl font-bold sm:mt-10 text-blue-500">So please refresh the page and
-              try again.</h1>
-
-          </section>
-        </div>}
-
+    <>
       <section className="category" >
         <div className="content" >
           <Slider {...settings}>
@@ -102,6 +67,6 @@ export const Category = () => {
           </Slider>
         </div>
       </section>
-    </div>
+    </>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Category } from "../../components/category/Category";
 import { CardHome } from "../../components/blog/CardHome";
 import Banner from "../../components/banner/Banner";
+import axiosInstance from "../../axios";
+import "./home.css"
 
 export const Home = () => {
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+    getCategory();
+  }, []);
+
+  const getCategory = async () => {
+    try {
+      await axiosInstance.get(`category`).then((res) => {
+        // console.log(res.data);
+        setCategory(res.data);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <>
-      <Banner />
-      <Category />
-      <CardHome />
+      <div style={{ position: "relative" }}>
+        {category.length === 0 &&
+
+          <div style={{ position: "absolute", zIndex: 10, width: "100%", height: "85vh", display: "flex", alignItems: "center" }}>
+            <section id="categoryWidth" style={{ backgroundColor: "black", color: "white", display: "flex", flexDirection: "column", gap: 20, borderRadius: 15, margin: "auto", justifyContent: "center", alignItems: "center", placeItems: "center" }} >
+
+              <h1 style={{ color: "red" }}>IMPORTANT!</h1>
+              <h3>You are seeing this message because the Backend Server is hosted on RENDER's FREE Tier Web Service and
+                it spin down after 15 minutes of inactivity.</h3>
+              <h3>Since there are no active users and I am using a Free Service, the first request might take longer to
+                respond.</h3>
+              <h2 style={{ color: "blue", marginTop: 10 }}>So please refresh the page and try again.</h2>
+
+            </section>
+          </div>
+        }
+      </div>
+      <div style={{opacity: category.length === 0 ? "10%" : "100%"}}>
+        <Banner />
+        <Category category={category} />
+        <CardHome />
+      </div>
     </>
   );
 };
